d41b73197df12c69ff774f0a1b23b86bd267ed1f
Andrey Evseev <a-evseev@yandex.ru>
2022-04-16 14:58:22 +0300

    сохранен код задачи 3

--- Seminar_01_Task_03/Program.cs
+++ Seminar_01_Task_03/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Семинар 1, задача 3
+
+int dayNum = 11;
+
+if (dayNum == 1)
+{
+    Console.WriteLine("Числу 1 соответствует день недели Понедельник");
+}
+else
+{
+    if (dayNum == 2)
+    {
+        Console.WriteLine("Числу 2 соответствует день недели Вторник");
+    }
+    else
+    {
+        if (dayNum == 3)
+        {
+            Console.WriteLine("Числу 3 соответствует день недели Среда");
+        }
+        else
+        {
+            if (dayNum == 4)
+            {
+                Console.WriteLine("Числу 4 соответствует день недели Четверг");
+            }
+            else
+            {
+                if (dayNum == 5)
+                {
+                    Console.WriteLine("Числу 5 соответствует день недели Пятница");
+                }
+                else
+                {
+                    if (dayNum == 6)
+                    {
+                        Console.WriteLine("Числу 6 соответствует день недели Суббота");
+                    }
+                    else
+                    {
+                        if (dayNum == 7)
+                        {
+                            Console.WriteLine("Числу 7 соответствует день недели Воскресенье");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Для числа {dayNum} нет соответствующего дня недели");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
