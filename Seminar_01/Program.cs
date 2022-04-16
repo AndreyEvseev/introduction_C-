137eb8fd62eb82eb914d138c394c6ec726f90dd6
Andrey Evseev <a-evseev@yandex.ru>
2022-04-16 12:54:11 +0300

    начальное задание добавлено в коммит

--- Seminar_01/Program.cs
+++ Seminar_01/Program.cs
@@ -5,7 +5,4 @@ number = -11;
 
 numberSquar = number * number;
 
-Console.Write("Квадрат ");
-Console.Write(number);
-Console.Write(" равен ");
-Console.WriteLine(numberSquar); 
+Console.WriteLine($"Квадрат {number} равен {numberSquar}");
