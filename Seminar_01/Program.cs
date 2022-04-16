ce3737621930cbc30767dd0e3fd392fcf681cd3d
Andrey Evseev <a-evseev@yandex.ru>
2022-04-16 12:14:13 +0300

    Начальный коммит

---
+++ Seminar_01/Program.cs
@@ -0,0 +1,11 @@
+﻿// код задачи № 1: вход-число, выход-квадрат числа
+
+int number, numberSquar;
+number = -11;
+
+numberSquar = number * number;
+
+Console.Write("Квадрат ");
+Console.Write(number);
+Console.Write(" равен ");
+Console.WriteLine(numberSquar); 
