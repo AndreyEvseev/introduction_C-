4643a9afaebc1badad81e525a7961e7345d19451
Andrey Evseev <a-evseev@yandex.ru>
2022-04-16 17:19:43 +0300

    код задания 7

---
+++ Seminar_01_Task_07/Program.cs
@@ -0,0 +1,9 @@
+﻿// Семинар 1, задача 7
+
+int number = 918;
+if (number > 999 && number < 99)
+{
+        Console.WriteLine("Ошибка! Введено не трехзначное число");
+}
+int digit = number % 10;
+Console.WriteLine(digit);
