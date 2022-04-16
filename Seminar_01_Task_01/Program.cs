616d17bec52560c597e5127d2705c973fbbacc59
Andrey Evseev <a-evseev@yandex.ru>
2022-04-16 14:20:55 +0300

    проверка является ли одно число квадратом другого

--- Seminar_01_Task_01/Program.cs
+++ Seminar_01_Task_01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Семинар 1, задача 1
+
+int number = -3;
+int numberSquar = 9;
+
+if (numberSquar == number * number)
+{
+    Console.WriteLine($"{numberSquar} является квадратом числа {number}");
+}
+else
+{
+    Console.WriteLine($"{numberSquar}  не является квадратом числа {number}");
+}
