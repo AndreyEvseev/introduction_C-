0ff2617dc3a3401afdfb49f7cd1fb11149cc3721
Andrey Evseev <a-evseev@yandex.ru>
2022-04-17 13:44:36 +0300

    Добавлен код задачи 4 семинара 2

---
+++ Seminar_02_Task_04/Program.cs
@@ -0,0 +1,7 @@
+﻿// 4. Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23
+
+int number = 161;
+int divisor1 = 7, divisor2 = 23;
+if(number % divisor1 == 0 && number % divisor2 == 0) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
