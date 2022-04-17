1bd827e20b6568c7d4a7dfc7f833b863d9ae4806
Andrey Evseev <a-evseev@yandex.ru>
2022-04-17 16:01:58 +0300

    Добавлен код задачи 6 семинара 2

---
+++ Seminar_02_Task_06/Program.cs
@@ -0,0 +1,12 @@
+﻿// 6. Создать массив на 3 элемента и присвоить 
+// рандомные значения элементам массива
+
+int N = 3, i = 0;
+int [] array = new int[N];
+while (i < N)
+{
+    array[i] = new Random().Next();
+    Console.Write(i + ": ");
+    Console.Write(array[i] + "; ");
+    i++;
+}
