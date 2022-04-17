7c4cabc1cda5858c7bc9d6fc3822845e74d2102d
Andrey Evseev <a-evseev@yandex.ru>
2022-04-17 13:03:33 +0300

    Оптимизирован код задачи 2 семинара 2

--- Seminar_02_Task_02/Program.cs
+++ Seminar_02_Task_02/Program.cs
@@ -2,12 +2,6 @@
 // и показывает наибольшую цифру числа
 
 int number2 = new Random().Next(10, 100);
-Console.WriteLine($"number2 = {number2};");
-if(number2 / 10 > number2 % 10)
-{
-    Console.WriteLine(number2 / 10);
-}
-else
-{
-    Console.WriteLine(number2 % 10);
-}
+Console.Write(number2 + ", ");
+if(number2 / 10 > number2 % 10) Console.WriteLine(number2 / 10);
+else Console.WriteLine(number2 % 10);
