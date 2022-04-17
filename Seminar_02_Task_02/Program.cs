74c0cbcd088bd8af2dbd6fbb8f3ae2969b3a7625
Andrey Evseev <a-evseev@yandex.ru>
2022-04-17 12:56:09 +0300

    добвален код задачи 2 семинара 2

---
+++ Seminar_02_Task_02/Program.cs
@@ -0,0 +1,13 @@
+﻿// 2. Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа
+
+int number2 = new Random().Next(10, 100);
+Console.WriteLine($"number2 = {number2};");
+if(number2 / 10 > number2 % 10)
+{
+    Console.WriteLine(number2 / 10);
+}
+else
+{
+    Console.WriteLine(number2 % 10);
+}
